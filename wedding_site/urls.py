da8db6aa58ae05b3bed524e3a7b4e5035ebde173
MrNurbek <xamrayevnurbek0@gmail.com>
2026-06-23 16:09:03 +0500

    new

--- wedding_site/urls.py
+++ wedding_site/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('invitation.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Media fayllar har doim ishlasin (DEBUG=False da ham)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT)
